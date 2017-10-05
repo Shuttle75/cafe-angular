86ce841ebcc13fd0e9a5214669e4b2b8cab50b0c
Shuttle75 <Alexey.Osadchiy@mail.ru>
2017-10-06 02:58:36 +0300

    Menu

---
+++ src/app/menu-group/menu-group.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+import { MenuService } from '../menu.service';
+import { MenuGroup, MenuItem } from '../menu';
+
+
+@Component({
+  selector: 'app-menu-group',
+  templateUrl: './menu-group.component.html',
+  styleUrls: ['./menu-group.component.css']
+})
+export class MenuGroupComponent implements OnInit {
+
+    @Input() menu_item_id: number;
+
+    menu_groups: MenuGroup[];
+    menu_items: MenuItem[];
+    error_message: string;
+
+
+    constructor(private menuService: MenuService, private route: ActivatedRoute, private router: Router) {
+
+    }
+
+    ngOnInit() {
+        this.menuService.getMenuGroups().subscribe(
+            response => this.menu_groups = response,
+            error => this.error_message = <any> error
+        );
+    }
+
+    goToDetails(group: MenuGroup) {
+        this.menuService.getMenuItemsByMenuGroup(group.id).subscribe(
+            response => this.menu_items = response,
+            error => this.error_message = <any> error
+        );
+    }
+
+}
